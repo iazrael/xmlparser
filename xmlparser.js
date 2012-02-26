f9f8d5201accb3df238df1f28240fe17c5095d88
azrael <azrael@imatlas.com>
2012-02-26 21:57:25 +0800

    build the basic code

---
+++ xmlparser.js
@@ -0,0 +1,18 @@
+
+;(function(undefined){
+    
+    var NODE_REGEX = /<([a-zA-Z][a-zA-Z0-9]+)\s+(.*?)(\/>)/;
+    
+    function XMLParser(){
+   
+    }
+    
+    XMLParser.prototype = {
+        parse: function(xmlText){
+            
+        }
+    }    
+    
+    window.XMLParser = XMLParser;
+    
+})();
