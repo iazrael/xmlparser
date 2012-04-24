867cbe715c9be41b7b702aec948c49172ab0f3bd
azrael <azrael@imatlas.com>
2012-04-24 20:59:39 +0800

    定义基本类
    Signed-off-by: azrael <azrael@imatlas.com>

--- xmlparser.js
+++ xmlparser.js
@@ -1,15 +1,73 @@
 
 ;(function(undefined){
     
+
     /**
      * 节点定义
      */
     function Node(){
-        this._attrs = {};
+        
     }
 
+    /*
+     * Node 类型常量定义
+     */
+    Node.ELEMENT_NODE = 1;
+    Node.ATTRIBUTE_NODE = 2;
+    Node.TEXT_NODE = 3;
+    Node.CDATA_SECTION_NODE = 4;
+    Node.COMMENT_NODE = 8;
+    Node.DOCUMENT_NODE = 9;
+    Node.DOCUMENT_TYPE_NODE = 10;
+
     Node.prototype = {
-        children: [],
+        nodeType: null,
+        nodeName: null,
+        nodeValue: null,
+        attributes: {},
+        childNodes: [],
+        hasAttributes: function(){
+            return !Util.isEmptyObject(this.attributes);
+        },
+        hasChildNodes: function(){
+            return !!this.childNodes.length;
+        }
+        //,TODO appendChild, cloneNode, insertBefore, insertAfter, removeChild, replaceChild, 
+        //normalize, contains
+        //
+    }
+
+    function Element(){
+
+    }
+
+    Element.prototype = {
+        tagName: null,
+        getAttribute: function(key){
+
+        },
+        setAttribute: function(key, value){
+
+        },
+        hasAttribute: function(key){
+
+        },
+        removeAttribute: function(key){
+
+        },
+        //============= 扩展的方法 ========================
+        /*getElementById: function(id){
+
+        },
+        getElementsByTagName: function(tagName){
+
+        },
+        getElementsByName: function(name){
+            
+        },
+        getElementsByClassName: function(className){
+
+        },*/
         /**
          * getAttribute和setAttribute的简写
          * @param  {String} key   
@@ -32,26 +90,10 @@
          */
         find: function(selector){
 
-        },
-        getAttribute: function(key){
-
-        },
-        setAttribute: function(key, value){
-
-        },
-        getElementById: function(id){
-
-        },
-        getElementsByName: function(name){
-            
-        },
-        getElementsByTagName: function(tagName){
-
-        },
-        getElementsByClassName: function(className){
-
         }
     };
+
+    Util.extend(Element, Node);
 
     /**
      * 文档定义
@@ -61,19 +103,10 @@
     }
 
     Document.prototype = {
-        getElementById: function(id){
-
-        },
-        getElementsByName: function(name){
             
-        },
-        getElementsByTagName: function(tagName){
-
-        },
-        getElementsByClassName: function(className){
-
-        }
     }
+    
+    Util.extend(Document, Element);
 
     function XMLParser(){
     
