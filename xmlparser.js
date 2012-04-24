62603bb8e5b397f4432dc2887de377930fcd28dc
azrael <azrael@imatlas.com>
2012-04-24 09:41:21 +0800

    initialize
    Signed-off-by: azrael <azrael@imatlas.com>

--- xmlparser.js
+++ xmlparser.js
@@ -1,15 +1,93 @@
 
 ;(function(undefined){
     
-    var NODE_REGEX = /<([a-zA-Z][a-zA-Z0-9]+)\s+(.*?)(\/>)/;
-    
+    /**
+     * 节点定义
+     */
+    function Node(){
+        this._attrs = {};
+    }
+
+    Node.prototype = {
+        children: [],
+        /**
+         * getAttribute和setAttribute的简写
+         * @param  {String} key   
+         * @param  {String} value @optional
+         * @return {String}, {Node}
+         */
+        attr: function(key, value){
+
+        },
+        /**
+         * getElement 系列方法的简写, 仅支持简易的一级选择器
+         * @param  {String} selector  
+         * @return {Array}, {Node}
+         * @example 
+         * find('#id');
+         * find('.class');
+         * find('tag');
+         * find('@attr');
+         * 
+         */
+        find: function(selector){
+
+        },
+        getAttribute: function(key){
+
+        },
+        setAttribute: function(key, value){
+
+        },
+        getElementById: function(id){
+
+        },
+        getElementsByName: function(name){
+            
+        },
+        getElementsByTagName: function(tagName){
+
+        },
+        getElementsByClassName: function(className){
+
+        }
+    };
+
+    /**
+     * 文档定义
+     */
+    function Document(){
+
+    }
+
+    Document.prototype = {
+        getElementById: function(id){
+
+        },
+        getElementsByName: function(name){
+            
+        },
+        getElementsByTagName: function(tagName){
+
+        },
+        getElementsByClassName: function(className){
+
+        }
+    }
+
     function XMLParser(){
-   
+    
     }
     
     XMLParser.prototype = {
+        /**
+         * 解析xml字符串的方法入口, 传入的xml字符串必须严格符合xml规范
+         * @param  {String} xmlText 
+         * @return {Document}
+         */
         parse: function(xmlText){
-            
+            var doc = new Document();
+            return doc;
         }
     }    
     
